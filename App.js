d2eb53c0f9870598e1419645d3a567527bfa4b5f
albae69 <ahmadalbaihaqi69@gmail.com>
2021-01-26 00:25:24 +0700

    interpolate

--- App.js
+++ App.js
@@ -7,16 +7,25 @@ export default function App() {
     useNativeDriver: true,
     toValue: {
       x: 150,
-      y: 300,
+      y: 250,
     },
-    delay: 2000,
+    delay: 1000,
   }).start();
+
+  const rotate = position.x.interpolate({
+    inputRange: [0, 50],
+    outputRange: ['0deg', '360deg'],
+  });
 
   return (
     <View style={{flex: 1, backgroundColor: 'white'}}>
       <Animated.View
         style={{
-          transform: [{translateX: position.x}, {translateY: position.y}],
+          transform: [
+            {translateX: position.x},
+            {translateY: position.y},
+            {rotate: rotate},
+          ],
           height: 80,
           width: 80,
           backgroundColor: 'lightgray',
